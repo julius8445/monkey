4ade5decffe1d089c5985be075eeff3a4abff696
Nicklous Grant <nicklous.julian.grant@gmail.com>
2023-05-30 00:39:14 -0400

    parser test setup

--- src/ast.rs
+++ src/ast.rs
@@ -1,31 +1,31 @@
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub struct Program {
     pub statements: Vec<Statement>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub enum Statement {
     Let(Let),
     Return(Return),
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub struct Let {
     pub name: Ident,
     pub value: Expression,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub struct Return {
     pub value: Expression,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub enum Expression {
     Ident(Ident),
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub struct Ident {
     pub value: String,
 }
--- src/parser.rs
+++ src/parser.rs
@@ -22,6 +22,31 @@ impl Parser {
         p
     }
 
+    pub fn parse_program(&mut self) -> Option<Program> {
+        let p = Program {
+            statements: vec![
+                Statement::Let(Let {
+                    name: Ident { value: "x".into() },
+                    value: Expression::Ident(Ident { value: "x".into() }),
+                }),
+                Statement::Let(Let {
+                    name: Ident { value: "y".into() },
+                    value: Expression::Ident(Ident { value: "x".into() }),
+                }),
+                /*Statement::Return(Return {
+                    value: Expression::Ident(Ident { value: "x".into() }),
+                }),*/
+                Statement::Let(Let {
+                    name: Ident {
+                        value: "foobar".into(),
+                    },
+                    value: Expression::Ident(Ident { value: "x".into() }),
+                }),
+            ],
+        };
+        None
+    }
+
     fn next_token(&mut self) {
         self.current_token = mem::replace(&mut self.peek_token, Token::Eof);
         self.peek_token = self.lexer.next().unwrap();
@@ -30,15 +55,35 @@ impl Parser {
 
 #[cfg(test)]
 mod test {
+    use crate::{
+        ast::{Let, Statement},
+        lexer::Lexer,
+    };
+
+    use super::Parser;
 
     #[test]
     fn test_let_statements() {
-        let _input = "
+        let input = "
             let x = 5;
             let y = 10;
+        
             let foobar = 838383;
         ";
 
-        let _identifiers = vec!["x", "y", "foobar"];
+        let identifiers = vec!["x", "y", "foobar"];
+
+        let mut p = Parser::new(Lexer::new(input.into()));
+
+        let program = p.parse_program().expect("parser should produce valid ast");
+
+        assert_eq!(program.statements.len(), 3);
+
+        for (i, stmt) in program.statements.iter().enumerate() {
+            let Statement::Let(s) = stmt else {
+                panic!("expected let statement. Found {stmt:?}");
+            };
+            assert_eq!(s.name.value, identifiers[i]);
+        }
     }
 }
